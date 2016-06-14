53da5122c296e8f2bb11134a55fd415bd74dbba3
Gabe Rodriguez <grod220@gmail.com>
2016-06-14 16:55:49 -0400

    static routing added

--- app.js
+++ app.js
@@ -1,34 +1,61 @@
 var express = require('express');
-var template = require('./template')
+var template = require('./template');
 var swig = require('swig');
 var app = express();
+var routes = require('./routes/');
 
-app.engine('html', swig.renderFile)
-
-app.set('view engine', 'html')
-app.set('views', __dirname + '/views')
-
+// Swig stuff
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 swig.setDefaults({ cache: false });
 
-app.get('/', function(req,res,next) {
-  res.render('index', template.locals);
+app.use('/', routes);
 
-});
+app.use(express.static('public'));
 
-app.get('/:userEntry', function(req,res,next) {
-  res.send(req.params.userEntry + '\n');
-  next();
-});
-
-app.use(function(req,res,next) {
-  console.log(Object.keys(req));
-  console.log(req.method + " " + req.url + " " + res.statusCode + "\n");
-  next();
-});
-
-app.use('/special/:anything', function(req,res,next) {
-  console.log("You are in a special place.");
-  res.end();
-});
 
 app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/:userEntry', function(req,res,next) {
+//   res.send(req.params.userEntry + '\n');
+//   next();
+// });
+
+// app.use(function(req,res,next) {
+//   console.log(Object.keys(req));
+//   console.log(req.method + " " + req.url + " " + res.statusCode + "\n");
+//   next();
+// });
+
+// app.use('/special/:anything', function(req,res,next) {
+//   console.log("You are in a special place.");
+//   res.end();
+// });
+
